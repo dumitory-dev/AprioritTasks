9a5d81dbff3456e9275642e5810386968d85b58d
Dmitry <blackmesa2018@gmail.com>
2019-08-16 19:46:59 +0300

    First task
    Write the code to find the factorial

---
+++ Factorial/Factorial/Factorial.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using ulong64 = unsigned long long;
+
+constexpr ulong64 get_factorial(const ulong64 val)
+{
+	ulong64 tmp_factorial{1};
+
+	for (ulong64 i{1}; i <= val; ++i)
+		tmp_factorial*=i;
+
+	return tmp_factorial;
+}
+
+
+int main(void) noexcept
+{
+	try
+	{
+		std::cout << get_factorial(5);
+		std::cin.get();
+	}
+	catch (...)
+	{
+		return -1;
+	}
+
+	return 0;
+    
+}
+
+
+
