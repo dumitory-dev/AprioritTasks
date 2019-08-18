3ce58448ba7827a0b126bcdf85a8ddfc657ce0c6
Dmitry <45358107+DumitoryDev@users.noreply.github.com>
2019-08-18 17:23:37 +0300

    Add new task
    Find the nearest element in the index vector and whose value is less than the element or the number passed to the function, return the index of the found element

---
+++ LowerNumber/LowerNumber/LowerNumber.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <vector>
+
+int find_lower_with_number(const std::vector<int> & data, const int num) noexcept
+{
+	const auto size = data.size();
+	for (auto i{0u}; i < size; ++i)
+		if (data[i] < num)
+			return i;
+		
+	return -1;
+	
+}
+
+
+int find_lower_with_index(const std::vector<int> & data, const size_t index) noexcept
+{
+	if (data.empty())
+		return -1;
+
+	if (data.size() == 1)
+		return 0;
+	
+	auto begin_index{static_cast<int>(index) - 1};
+	
+	while (static_cast<int>(index) != begin_index)
+	{
+		if (begin_index < 0){
+			begin_index = static_cast<int>(data.size()) - 1;
+			continue;
+		}
+				
+		if (data[begin_index] < data[index])
+			return begin_index;
+
+		--begin_index;
+				
+	}
+	   		
+	return -1;
+	
+}
+
+
+
+int main(void) noexcept
+{
+	try
+	{
+		std::vector v{2,9,6,3,8,1,9,0};
+
+		std::cout << find_lower_with_index(v,5) << std::endl;
+		std::cout << find_lower_with_number(v,5) << std::endl;
+
+		return 0;
+	}
+	catch (...)
+	{
+		return -1;
+	}
+  
+}
+
