3d644871952e01977db80e47672af97d44991952
Dmitry <45358107+DumitoryDev@users.noreply.github.com>
2019-08-20 17:23:28 +0300

    Palindrome string check
    Palindrome string check

---
+++ CheckPalindrome/CheckPalindrome/CheckPalindrome.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+
+bool check_palindrome(const std::string_view str)
+{
+
+	auto start = std::cbegin(str);
+	auto end = --std::cend(str);
+
+	while (true)
+	{
+		if (*start != *end)
+			return false;
+
+		if (start == end)
+			return true;
+
+		++start;
+		--end;
+
+	}
+
+}
+
+
+int main(void)
+{
+	try
+	{
+		
+		const auto str("1211");
+		std::cout << std::boolalpha << check_palindrome(str);
+
+		return 0;
+	}
+	catch (...)
+	{
+		return -1;
+	}
+
+
+}
+
